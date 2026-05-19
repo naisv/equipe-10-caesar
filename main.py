182b319303025061434123a401f4f82d0f7bf05e
Paul Serra <paul.serra.1@ens.etsmtl.ca>
2026-05-19 15:37:14 -0400

    ajout de la fonction dechiffrage enigma (à tester en fonction de la fonction dechiffrage cesar)

--- main.py
+++ main.py
@@ -59,6 +59,12 @@ def enigma_chiffrer(message: str, cles):
 	# Exemple attendu par le test :
 	# - enigma_chiffrer("MAISON", (7, 16, 9)) -> "TQRZEW"
 	pass
+def enigma_dechiffrer(message: str, cles):
+	dechiffrage=""
+	for position in len(message):
+		indice_cle=position%3 #permet d'identifier quelle clé du tuple cles il faut utiliser
+		dechiffrage+=dechiffrer(message[position],cles[indice_cle]) #Chiffre la lettre du message avec la bonne clé
+	return dechiffrage
 
 
 def _parse_cle(texte: str):
